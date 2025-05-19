11e5cf350524a14f485be00f95cff2e14eef779b
Qbeen2 <thdrbqls521@gmail.com>
2025-05-19 10:36:02 +0900

    Feat: adding eureka client settings and CI component

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ from fastapi import FastAPI
 from app.api.v1 import agentic
 from app.config.logging_config import setup_logging
 from fastapi.middleware.cors import CORSMiddleware
+from eureka_client import eureka_client
+from os import getenv
+
 # main.py (혹은 uvicorn 실행에 사용되는 시작 파일)
 import asyncio
 import sys
@@ -36,7 +39,19 @@ app.include_router(agentic.router, prefix="/api/v1")
 @app.on_event("startup")
 async def startup_event():
     logger.info("[WORKFLOW] Server started successfully")
+    await eureka_client.init_async(
+        eureka_server=getenv("EUREKA_IP","http://localhost:8761/eureka"),
+        app_name=getenv("EUREKA_APP_NAME","eum-classifier"),
+        instance_host=getenv("EUREKA_HOST","localhost"),
+        instance_port=int(getenv("EUREKA_PORT","8000"))
+    )
+
 
 @app.on_event("shutdown")
 async def shutdown_event():
     logger.info("[WORKFLOW] Server shutting down")
+    await eureka_client.stop_async()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
